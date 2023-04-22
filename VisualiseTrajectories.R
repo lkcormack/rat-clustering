8e063a20d67ead416b442cc652c9cffe2d65c753
Lawrence K Cormack <cormack@utexas.edu>
2023-04-22 12:44:29 -0500

    added some notes on data cleaning

--- VisualiseTrajectories.R
+++ VisualiseTrajectories.R
@@ -2,5 +2,15 @@
 
 library(tidyverse)
 
-# load files
+# Define the file path
+file_path <- "./data/3Rats/Average_Position_01/rat1_avg.csv"
 
+# Read the .csv file into a data frame, skipping the first row
+xyt_data <- read_csv(file_path, col_names = c("frame", "x", "y"), skip = 1)
+
+# remove leading and trailing rows with NaNs
+xyt_data <- xyt_data %>%
+  slice(which.max(!is.na(x) & !is.na(y)):n())
+
+# Print das tibble
+print(xyt_data)
