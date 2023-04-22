f66ff9ac2855f3f866b3b06c79487ad2f87ce513
Lawrence K Cormack <cormack@utexas.edu>
2023-04-22 09:12:27 -0500

    Create MoveRatFiles.sh
    Made a shell script to copy the raw data (average position) files into the untracked local data folder.

---
+++ MoveRatFiles.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Set the source and destination directories
+source_dir="/Users/lkc3-admin/Library/CloudStorage/Box-Box/MovementAnalysisData_DLC/3_completed"
+dest_dir="/Users/lkc3-admin/Documents/GitHub/rat-clustering/data/3Rats"
+
+# Initialize a counter
+counter=1
+
+# Loop through each subdirectory in the source directory
+for sub_dir in "${source_dir}"/*/Average_Position; do
+    # Check if the subdirectory exists
+    if [ -d "${sub_dir}" ]; then
+        # Copy the subdirectory to the destination directory with the new name
+        cp -r "${sub_dir}" "${dest_dir}/Average_Position_$(printf "%02d" "${counter}")"
+        # Increment the counter
+        counter=$((counter + 1))
+    fi
+done
