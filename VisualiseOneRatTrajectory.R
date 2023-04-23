bf7a5ba723477fba1ff58f97df98dd144d235984
Lawrence K Cormack <cormack@utexas.edu>
2023-04-23 12:56:18 -0500

    Update VisualiseOneRatTrajectory.R
    Got some 3D trajectory plotting going.

--- VisualiseOneRatTrajectory.R
+++ VisualiseOneRatTrajectory.R
@@ -1,16 +1,30 @@
 # Visualize the rat data in x,y,t space
 
 library(tidyverse)
+library(plotly)
 
 # Define the file path
 file_path <- "./data/3Rats/Average_Position_01/rat1_avg.csv"
 
 # Read the .csv file into a data frame, skipping the first row
-xyt_data <- read_csv(file_path, col_names = c("frame", "x", "y"), skip = 1)
+xyt_data_raw <- read_csv(file_path, col_names = c("frame", "x", "y"), skip = 1)
 
-# remove leading and trailing rows with NaNs
-xyt_data <- xyt_data %>%
-  slice(which.max(!is.na(x) & !is.na(y)):n())
+# remove leading with NaNs
+xyt_dat <- xyt_data_raw %>%
+  filter(!is.na(x) & !is.na(y)) # drop rows with NaN positions
+
+xyt_dat <- xyt_dat %>% 
+  mutate(f_diff = c(1, diff(frame)))
 
 # Print das tibble
-print(xyt_data)
+#print(xyt_dat)
+
+fig <- xyt_dat %>% 
+  plot_ly(x = ~x, y = ~y, z = ~frame, type = 'scatter3d', mode = 'lines',
+               opacity = 1)
+# , line = list(width = 6, color = ~color, reverscale = FALSE))
+
+fig
+
+# plot_ly(x=xyt_dat$x, y=xyt_dat$y, z=xyt_dat$frame, type="scatter3d",
+#        mode="lines")
