5935699f02b71be639c2e971428d7888d855fdd1
Lawrence K Cormack <cormack@utexas.edu>
2023-04-23 20:49:55 -0500

    updated handling multiple rats
    Combines 3 rates and keeps only data where all three rats have valid numbers. Also improved x,y,t plotting.

--- CombineRatDataInASession.R
+++ CombineRatDataInASession.R
@@ -1,22 +1,50 @@
 library(tidyverse)
 
 # set directory path
-dir_path <- "/path/to/directory/"
+dir_path <- "./data/3Rats/Average_Position_01"
 
 # get list of csv files in directory
 file_list <- list.files(path = dir_path, pattern = ".csv", full.names = TRUE)
+n_files <- length(file_list)
 
-# create a function to read csv file and add identifier column
-read_csv_add_id <- function(file_path, id) {
-  read_csv(file_path) %>% 
-    mutate(frame_id = id)
+xyt_dat = data.frame()
+
+# now read the files, tacking them on as we go
+for (i in 1:n_files) {
+  tmp <-  read_csv(file_list[i], col_names = c("frame", "x", "y"), skip = 1)
+  tmp <- tmp %>% 
+    #filter(!is.na(x) & !is.na(y)) %>% 
+    mutate(rat_num = i)
+  xyt_dat <- rbind(xyt_dat, tmp)
 }
 
-# use purrr::map_df to apply function to each file in list and combine into one tibble
-combined_data <- map_df(file_list, read_csv_add_id, .id = "file_id")
+nan_frames = xyt_dat[is.na(xyt_dat$x) & is.na(xyt_dat$y), 'frame']
 
-# rename columns to desired names
-names(combined_data) <- c("file_id", "frame", "x", "y", "frame_id")
+xyt_dat <- xyt_dat[!xyt_dat$frame %in% nan_frames$frame, ]
 
-# view combined data
-combined_data
+xyt_dat <- xyt_dat %>%
+  mutate(f_diff = c(1, diff(frame)))
+
+xyt_dat$rat_num <- as.factor(xyt_dat$rat_num)
+  
+## Plotting ##
+n_frms_plt = 10000
+b_frms_plt = 1
+e_frms_plt = b_frms_plt + n_frms_plt
+z_range = c(xyt_dat$frame[b_frms_plt], xyt_dat$frame[e_frms_plt])
+
+fig <- xyt_dat %>% 
+  plot_ly(x = ~x, y = ~y, z = ~frame, color = ~rat_num,
+          type = 'scatter3d', mode = 'lines', 
+          colors = c("blue", "green", "red"),
+          opacity = 0.3, 
+          line = list(width = 6, opacity = 0.3)) %>% 
+  layout(title = "Rats!",
+         scene = list(
+           xaxis = list(title = "x position"),
+           yaxis = list(title = "y position"),
+           zaxis = list(title = "video frame", range = z_range)
+           ))
+
+show(fig)
+
--- VisualiseOneRatTrajectory.R
+++ VisualiseOneRatTrajectory.R
@@ -9,7 +9,7 @@ file_path <- "./data/3Rats/Average_Position_01/rat1_avg.csv"
 # Read the .csv file into a data frame, skipping the first row
 xyt_data_raw <- read_csv(file_path, col_names = c("frame", "x", "y"), skip = 1)
 
-# remove leading with NaNs
+# remove rows with NaNs
 xyt_dat <- xyt_data_raw %>%
   filter(!is.na(x) & !is.na(y)) # drop rows with NaN positions
 
