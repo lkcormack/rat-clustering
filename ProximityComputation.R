aaeefc5d722f2ad71d9091661fed4a8a1f252aee
Lawrence K Cormack <cormack@utexas.edu>
2023-04-24 20:22:58 -0500

    a bit more on dbscan clustering

--- ProximityComputation.R
+++ ProximityComputation.R
@@ -19,7 +19,7 @@ dist_df <- xyt_dat %>%
 # note that thresholding on this number is equivalent to
 # saying all 3 rats were within a vesca piscis; the intersection
 # of two circles around two rats of that diameter
-thresh = 250 # 
+thresh = 300 # 
 
 dist_df <- dist_df %>% 
   mutate(cluster = dist < thresh)
--- dbscanClustering.R
+++ dbscanClustering.R
@@ -26,13 +26,20 @@ results <- xyt_dat %>%
   group_by(frame) %>%
   group_modify(~ perform_dbscan(.x, min_objects = min_objects, eps = eps))
 
-# Print the results
-print(results)
-
 # plot of some sort
-ggplot(results, aes(x = frame, y = cluster, color = cluster)) +
+all_dat_plot <- results %>% 
+ggplot(aes(x = frame, y = cluster, color = cluster)) +
   geom_jitter(size = 2, alpha = 0.1)
   
+show(all_dat_plot)
 
+# make a tibble with just the clustured data
+clustered_data <- results[results$cluster != 0,]
 
+# plot of some sort
+clstr_dat_plot <- clustered_data %>% 
+  ggplot(aes(x = frame, y = cluster, color = cluster)) +
+  geom_jitter(size = 2, alpha = 0.1)
+
+show(clstr_dat_plot)
 
