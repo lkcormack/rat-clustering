2f4db280728bb5db69957791f6fff676ee05d82f
Lawrence K Cormack <cormack@utexas.edu>
2023-05-31 14:48:51 -0500

    Update MoveRatFiles.sh

--- MoveRatFiles.sh
+++ MoveRatFiles.sh
@@ -3,8 +3,8 @@
 nRats=15
 
 # Set the source and destination directories
-source_dir="/Users/lkc3-admin/Library/CloudStorage/Box-Box/MovementAnalysisData_DLC/${nRats}_completed"
-dest_dir="/Users/lkc3-admin/Documents/GitHub/rat-clustering/data/${nRats}Rats"
+source_dir="/Users/lkc/Library/CloudStorage/Box-Box/MovementAnalysisData_DLC/${nRats}_complete"
+dest_dir="/Users/lkc/Documents/GitHub/rat-clustering/data/${nRats}Rats"
 
 # Initialize a counter
 counter=1
