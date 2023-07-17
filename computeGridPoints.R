49583a3d7bb6ffb3413424321cb4e991e5358856
Lawrence K Cormack <cormack@utexas.edu>
2023-07-17 09:59:21 -0500

    aesthetic improvements to computeGridPoints.R

--- computeGridPoints.R
+++ computeGridPoints.R
@@ -4,8 +4,10 @@ n <- 3  # number of points
 grid_size <- ceiling(sqrt(n)) + 1
 
 # Create all possible grid points, leaving out the edges
-grid_points <- expand.grid(x = seq(1000/(grid_size-1), 1000 - 1000/(grid_size-1), length.out = grid_size - 2), 
-                           y = seq(1000/(grid_size-1), 1000 - 1000/(grid_size-1), length.out = grid_size - 2))
+grid_points <- expand.grid(x = seq(1000/(grid_size-1), 
+                                   1000 - 1000/(grid_size-1), length.out = grid_size - 2), 
+                           y = seq(1000/(grid_size-1), 
+                                   1000 - 1000/(grid_size-1), length.out = grid_size - 2))
 
 # If there are more grid points than needed, select a subset
 if (nrow(grid_points) > n) {
