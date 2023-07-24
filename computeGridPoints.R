35b5b8331cfe13f3d059921608ae3d0da4510a98
Lawrence K Cormack <cormack@utexas.edu>
2023-07-24 10:08:42 -0500

    Update computeGridPoints.R
    Embellished le comments.

--- computeGridPoints.R
+++ computeGridPoints.R
@@ -1,17 +1,17 @@
 n <- 6  # number of points
 
-# Calculate the size (side_length) of the grid
-# need the central part of the grid to hold
-# whatever the next perfect square is above n
-grid_size <- ceiling(sqrt(n)) # grid is grid_size x grid_size
+# Calculate the size (side length) of the grid.
+# We need the central part of the grid to hold
+# whatever the next perfect square is above n.
+grid_size <- ceiling(sqrt(n)) # grid is grid_size x grid_size big
 
-# Create all possible grid points, leaving out the edges
+# Calculate grid coordinates.
 grid_points <- expand.grid(x = seq(0, 1000, 
                                    length.out = grid_size), 
                            y = seq(0, 1000, 
                                    length.out = grid_size))
 
-# If there are more grid points than needed, select a subset
+# If there are more grid points than needed, select a subset.
  if (nrow(grid_points) > n) {
    set.seed(42)  # set seed for reproducibility
    points <- grid_points[sample(nrow(grid_points), n), ]
