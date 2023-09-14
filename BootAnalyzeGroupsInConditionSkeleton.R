b02a4afbb0bd8fc0c5759afc2195c91057f7f4cd
Lawrence K Cormack <cormack@utexas.edu>
2023-09-14 13:54:21 -0500

    Got boot skeleton running

--- BootAnalyzeGroupsInConditionSkeleton.R
+++ BootAnalyzeGroupsInConditionSkeleton.R
@@ -1,10 +1,13 @@
 # Within condition bootstapping
 
-install.packages(c("tidyverse", "fs"))
+# install.packages(c("tidyverse", "fs"))
 library(tidyverse)
+library(rstudioapi)
 library(fs)
 
-root_dir <- choose.dir()
+root_dir <- selectDirectory(caption = "Select Directory",
+                            label = "Select",
+                            path = getActiveProject())
 
 sub_dirs <- dir(root_dir, full.names = TRUE, recursive = FALSE)
 
@@ -13,9 +16,16 @@ all_files <- list()
 
 # Loop through each subdirectory
 for (sub_dir in sub_dirs) {
-  
+  #print("getting subdirectories")
+  #print(sub_dir)  # for debugging
   # Get a list of files in the current subdirectory
-  files_in_current_dir <- dir(sub_dir, full.names = TRUE)
+  #files_in_current_dir <- dir(sub_dir, full.names = TRUE)
+  # load the list of files in this directory
+  files_in_current_dir <-
+    list.files(path = sub_dir,
+               pattern = ".RData",
+               full.names = TRUE)
+  n_files <- length(files_in_current_dir)
   
   # Append this list to the main list
   all_files <- c(all_files, files_in_current_dir)
@@ -27,11 +37,13 @@ num_iterations <- 3  # should go up on TACC
 results <- vector("list", num_iterations)
 
 for(i in 1:num_iterations) {
+  
   sampled_files <- sample(all_files, n, replace = TRUE)
   
+  
   # Load sampled data
-  sampled_data <- lapply(sampled_files, read.csv) %>% 
-    bind_rows() 
+#  sampled_data <- lapply(sampled_files, read.csv) %>% 
+#    bind_rows() 
   
   # DBSCAN
   # ... 
