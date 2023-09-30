8ac811fc6627ce0859097db0532ad0f8c39cecb3
Lawrence K Cormack <cormack@utexas.edu>
2023-09-30 13:39:13 -0500

    Added boring utility scripts

---
+++ KS_Test_Fest.R
@@ -0,0 +1,21 @@
+ks3vs6lens <- ks.test(n3rats_lens, n6rats_lens)
+ks3vs9lens <- ks.test(n3rats_lens, n9rats_lens)
+ks3vs15lens <- ks.test(n3rats_lens, n15rats_lens)
+ks6vs9lens <- ks.test(n6rats_lens, n9rats_lens)
+ks6vs15lens <- ks.test(n6rats_lens, n15rats_lens)
+ks9vs15lens <- ks.test(n9rats_lens, n15rats_lens)
+
+save(ks3vs6lens, ks3vs9lens, ks3vs15lens, 
+     ks6vs9lens, ks6vs15lens, ks9vs15lens, 
+     file = "KStestsForLens.RData")
+
+ks3vs6vals <- ks.test(n3rats_vals, n6rats_vals)
+ks3vs9vals <- ks.test(n3rats_vals, n9rats_vals)
+ks3vs15vals <- ks.test(n3rats_vals, n15rats_vals)
+ks6vs9vals <- ks.test(n6rats_vals, n9rats_vals)
+ks6vs15vals <- ks.test(n6rats_vals, n15rats_vals)
+ks9vs15vals <- ks.test(n9rats_vals, n15rats_vals)
+
+save(ks3vs6vals, ks3vs9vals, ks3vs15vals, 
+     ks6vs9vals, ks6vs15vals, ks9vs15vals, 
+     file = "KStestsForVals.RData")
---
+++ LoadAllSumData.R
@@ -0,0 +1,13 @@
+load("6RatsClusterSummary.RData")
+n3rats_vals <- cluster_lengths_sizes$values
+n3rats_lens <- cluster_lengths_sizes$lengths
+load("6RatsClusterSummary.RData")
+n6rats_lens <- cluster_lengths_sizes$lengths
+n6rats_vals <- cluster_lengths_sizes$values
+load("9RatsClusterSummary.RData")
+n9rats_lens <- cluster_lengths_sizes$lengths
+n9rats_vals <- cluster_lengths_sizes$values
+load("15RatsClusterSummary.RData")
+n15rats_lens <- cluster_lengths_sizes$lengths
+n15rats_vals <- cluster_lengths_sizes$values
+
