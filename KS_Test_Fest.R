ebfa404684aac72d458e68d4a767c8606ab6b4b4
Lawrence K Cormack <cormack@utexas.edu>
2023-11-27 15:29:09 -0600

    Update KS_Test_Fest.R

--- KS_Test_Fest.R
+++ KS_Test_Fest.R
@@ -1,3 +1,6 @@
+# goes with LoadAllSumData.R
+# you have to run that first to get the data
+
 ks3vs6lens <- ks.test(n3rats_lens, n6rats_lens)
 ks3vs9lens <- ks.test(n3rats_lens, n9rats_lens)
 ks3vs15lens <- ks.test(n3rats_lens, n15rats_lens)
@@ -19,3 +22,4 @@ ks9vs15vals <- ks.test(n9rats_vals, n15rats_vals)
 save(ks3vs6vals, ks3vs9vals, ks3vs15vals, 
      ks6vs9vals, ks6vs15vals, ks9vs15vals, 
      file = "KStestsForSizes.RData")
+
