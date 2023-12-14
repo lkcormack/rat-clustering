69898bc05473143fe18c7e09aa6ca29b2b4621df
Lawrence K Cormack <cormack@utexas.edu>
2023-12-13 22:53:48 -0600

    more house cleaning and documenting
    See especially file_descriptions.md

---
+++ BootMotherAnalyzeGroupsInCond.R
@@ -0,0 +1,270 @@
+args <- commandArgs(trailingOnly = TRUE)
+cat("Received arguments:", args, "\n")
+arg_list <- list()
+for (arg in args) {
+  arg_parts <- unlist(strsplit(arg, "="))
+  if (length(arg_parts) == 2) {
+    arg_name <- arg_parts[1]
+    arg_value <- arg_parts[2]
+    arg_list[[arg_name]] <- arg_value
+  }
+}
+
+# Access arguments as a list
+cond <- arg_list[["--cond"]]
+iterations <- arg_list[["--iter"]]
+
+folder_path <- '/scratch/09634/michaelp3844/Run/data'
+save_path <- '/scratch/09634/michaelp3844/'
+
+###### Within condition bootstapping
+# Bootstrap analysis of groups in a condition
+# This code (will...) 
+#
+#  let the user pick a condition (number of rats)
+#  Need to change this to a hard-coded path for running one Lonestar6
+#
+# loop through bootstrap iterations
+#  loop through the runs in that condition
+#     make a random sample of rat data from all runs
+#     load each rat's .RData file
+#     combine the rat data files into one data frame
+#     compute the group lengths
+#     compute a histogram of the lengths
+#     add the histogram to list 
+#  save out the histogram data for this condition
+
+# NOTE: MakeDataFilesFromCSVs.R must be used to create
+# the .RData files in the directories before running this!
+
+##### library loading ######
+library(tidyverse)
+library(rstudioapi) # won't be needed for Lonestar6
+library(fs)         # may not be needed anymore
+library(fpc)
+#############
+
+##### Do we save and/or plot?
+save_flag = TRUE # save out the rle results?
+plot_flag = FALSE # make plot? 
+
+######### function definitions ##################
+# Define the function to perform DBSCAN clustering
+# It takes the group labels (0 for no group), and adds
+# them on as additional column
+perform_dbscan <- function(data, min_objects, eps) {
+  # Perform DBSCAN clustering
+  cluster_result <- dbscan(data[, c("x", "y")], eps = eps, MinPts = min_objects)
+  
+  # Assign cluster labels to new "cluster" column
+  data$cluster <- cluster_result$cluster
+  data$iscore <- cluster_result$isseed
+  
+  return(data)
+}
+######### end function definitions ##################
+
+
+############ directory selection #################
+
+###get path to condition directory #######
+# root_dir <- selectDirectory(caption = "Select Directory",
+#                             label = "Select",
+#                             path = getActiveProject())
+
+# office
+#root_dir = "/Users/lkc/Documents/GitHub/rat-clustering/data/3Rats/"
+# laptop
+root_dir = folder_path
+dir_list <- dir(root_dir, full.names = TRUE, recursive = TRUE)
+
+# Initialize an empty list to hold all the files
+all_files <- dir_list
+
+
+######### number of bootstrap repetitions to run ############
+num_iterations <- iterations  # should go up on TACC
+
+############### Initialize storage #################
+# cluster_data_list <- vector("list", num_iterations)
+# hist_data_list <- vector("list", num_iterations)
+rle_data_list <- vector("list", num_iterations)
+rle_raw <- tibble() # empty tibble to hold rle results
+
+n_files <- cond
+############### Here's the big bootstrapping loop ############
+for(i in 1:num_iterations) {
+  
+  print(paste0("starting iteration ", i))
+  
+  # create an empty data frame to hold the combined data
+  sampled_data = data.frame()
+  
+  # replace = false prevents identical rats in a run 
+  # the bootstrapping is still valid because we're subsampling
+  sampled_files <- sample(all_files, n_files, replace = FALSE)
+  
+  
+  ###### Load sampled .RData files
+  
+  #### Okay, this is going to be clumsy as fuck, but...
+  #### because the runs are different lengths ...
+  ### we need to 
+  ### load the three files once to get 
+  ### their lengths, and then 
+  ### load 'em again to load 'em...
+
+  # vector to hold the lengths
+  run_lengths = vector()
+  # load the .RData files for the rats in this run
+  for (j in 1:length(sampled_files)) {
+    # print(paste("Loading rat", j)) # for debugging
+    # combine into one data frame
+    load(sampled_files[[j]])  # new xyt_dat data frame now on board
+    run_lengths = c(run_lengths, nrow(xyt_dat))
+  } # end of looping through files for this run
+  
+  min_run_length = min(run_lengths) # need to truncate all data to this value
+
+  # load the .RData files for the rats in this run
+  for (j in 1:length(sampled_files)) {
+    # print(paste("Loading rat", j)) # for debugging
+    # combine into one data frame
+    load(sampled_files[[j]])  # new xyt_dat data frame now on board
+    # will need to save these when we analyze social hierarchy...
+    id_string <- sub(".*/(Rat\\d+Run\\d+).*", "\\1", sampled_files[[j]])
+    xyt_dat$rat_num = j # set rat ID to an int
+    # print(nrow(xyt_dat))
+    sampled_data <- rbind(sampled_data, xyt_dat[1:min_run_length, ])
+    
+  } # end of looping through files for this run
+  
+  # rename the combined file back to xyt_dat
+  xyt_dat <- sampled_data
+  
+  ##### NA removal #########################################
+  # Find the frames with NaNs in either data column
+  nan_frames = xyt_dat[is.na(xyt_dat$x) | is.na(xyt_dat$y), 'frame']
+  
+  # Keep the frames that are *not* a member of nan_frames
+  # this will omit a rat's data for a given frame if one of its
+  # buddies has a NA on that frame, even if the first rat's data is valid...
+  # Which is what we need.
+  xyt_dat <- xyt_dat[!xyt_dat$frame %in% nan_frames$frame, ]
+ 
+  ##### run DBScan #########################################
+  # Set parameters
+  min_objects <- 3 # Minimum number of objects in a cluster
+  eps <- 100       # Maximum distance between two samples for
+  # them to be considered as in the same neighborhood
+
+  # preform the clustering on each video frame
+  # Group data by time and apply the perform_dbscan function
+  xyt_dat <- xyt_dat %>%
+    group_by(frame) %>% # group data by frame number
+    group_modify(~ perform_dbscan(.x,
+                                  min_objects = min_objects,
+                                  eps = eps))
+
+  ##### run rle analysis ##################################
+  # make data frame without unneeded columns
+  cluster_dat <- xyt_dat %>% select(rat_num, frame, cluster)
+  # rat number cycles fast, frame cycles slowly
+
+  # remove unneeded Big Kahuna data frame
+  #rm('xyt_dat')
+
+  # pivot such that
+  # - rats are rows,
+  # - frames are columns,
+  # - and entries are cluster ID number
+  # this will allow us to detect frame-to-frame continuity of clusters more
+  # easily
+  grps_tibble <- cluster_dat %>%
+    pivot_wider(names_from = frame, values_from = cluster)
+
+  # nrow(cluster_dat) == nrow(grps_tibble)*ncol(grps_tibble) - n_files
+  # this test passes!
+  
+  # convert to a matrix so we can do maths more directly
+  grps_matrix <- as.matrix(grps_tibble[,-1])
+  mat_dims <- dim(grps_matrix)
+  n_frames = mat_dims[2]
+
+  # length(grps_matrix) == nrow(cluster_dat)
+  # this test passes (length() for a matrix is number of elements)
+  
+  # get maximum integer group label for the `for()` loop below
+  max_grp_number <- max(grps_matrix) 
+
+  # only look for groups if they're are any!
+  if (max_grp_number > 0) {
+    
+    # # Initialize group label x frame array for group member counts
+    member_counts <- array(0, dim=c(max_grp_number, n_frames))
+    
+    # fill group label x frame array whose values are the number of
+    # members in that group
+    
+    for (j in 1:max_grp_number) {    # loop through the groups labels
+      temp <- grps_matrix    # make a matrix whose entries are
+      temp[temp == j] <- 1   # 1 for this group and
+      temp[temp != j] <- 0.  # 0 for the other groups
+      member_counts[j, ] <- colSums(temp) # number of members of this group for each frame
+    }
+    # We now have a matrix indicating how many rats (entry) are in a group (row) 
+    # on a given frame (column)
+    
+    # Now perform run length encoding (rle) to get a data frame of group lengths
+    # (in frames) and group sizes (# of rats).
+    # NB: doing this in separate `for()` loop from above for clarity.
+    for (j in 1:max_grp_number) {    # loop through the groups labels
+      # Perform the run-length encoding for this row
+      rle_output <- rle(member_counts[j, ])
+      
+      # the output of rle() is a list, so
+      # convert the RLE result to a temporary tibble
+      rle_tibble <- tibble(
+        lengths = rle_output$lengths,
+        values = rle_output$values,
+        grp_label = j,
+        run_label = i
+      )
+      # Note!! The "grp_label" refers to the group number we're running the 
+      # rle on. So runs of 0s will still have a label >= 1
+      
+      # Append to the results to the main output tibble
+      rle_raw <- bind_rows(rle_raw, rle_tibble)
+      
+    }
+    
+  } # if
+
+  # maybe add a cumulative sum column of the lengths
+  # to code the frame number at which clusters start
+  
+  rle_data_list[[i]] <- rle_raw
+
+  # cl_lgth_sz_temp <- rle_tibble[rle_tibble$values != 0, ]
+  # if (nrow(cl_lgth_sz_temp) > 0) {
+  #   hist_data_list[[i]] <- hist(cl_lgth_sz_temp$lengths, 30)
+  # }
+  # else {
+  #   hist_data_list[[i]] <- "nope"
+  # }
+
+  print(paste0("done with iteration ", i))
+  
+} 
+################# end of bootstrapping loop! ################
+
+##### Saving
+if (save_flag) {
+  # assemble a file name
+  path = paste(save_path, n_files, sep="/")
+  fname_str <- paste0(n_files, "MotherRatsBootSummary.RData") 
+  
+  # save out the data frame for this condition as a .RData file
+  save(rle_data_list,   # rle output with only actual groups
+       file = fname_str)
+}
---
+++ file_descriptions.md
@@ -0,0 +1,49 @@
+## Working file descriptions for the rat clustering code
+
+Key parts will get incorporated to the README.md for the repo.
+
+---
+
+### Look at real rat data in steps
+
+The following the files do a step-by-step analysis of a single run (recording) of the rat data
+
+####  01CombineRatDataInARun.R
+
+Takes all the data from a single "run" or recording, and combines them into a single data frame with a rat ID column.
+
+#### 02DBScanClustering.R
+
+This does the DBScan clustering, and outputs data in the same format as from the above step, except that it adds two columns:
+
+* a “cluster” column that gives a cluster ID value for each rat on each frame
+* a “seed” column that indicates whether a rat was in the interior of a cluster
+
+#### 03ComputeGroupStats.R
+
+This does the run length encoding (RLE) to determine group sizes and group lengths. It outputs a tibble containing columns with the group sizes (“values”), group lifetimes (“lengths”), and group IDs (“grp_label” – prob. not that useful here).
+
+### Look at real data in one go
+
+#### AnalyzeGroupsInCondition.R
+
+This combines the steps above into one script. The one caveat being that you have to convert the .csv data files to .RData files first using `MakeDataFilesFromCSVs.R` to make the loading faster.
+
+* **MakeDataFilesFromCSVs.R** – converts .csv files into .RData files
+
+---
+
+### Bootstrapping analysis
+
+Two basic bootstrapping analyses were run. In the first, a simulation of a 3 rat run, for example, consisted only of trajectories from rats that were run in a 3 rat condition. In the other (the “mother” bootstrap), a simulation of a 3 rat run, for example, could consist of trajectories from rats that were run in any condition (3, 6, 9, or 15 rats). The idea here was to see if there were grouping differences based solely on how rats moved with different numbers of rats in the environment, independent of any particular social behavior of one rat reacting to the presence of another.
+
+#### BootAnalyzeGroupsInCondition.R
+
+This is the main file that chews through the original data, and constructs bootstrap replicates based on actual rat trajectories, but that we’re not from the same actual run (recording). In other words, each “rat” in the bootstrap run could not have adjusted its behavior based on the other “rats” in the bootstrapped trial. The rats were, in effect, ghost rats that could not see, smell, or feel one another.
+
+In this version, all rat trajectories come from the same condition. In other words, for a “3 rats” bootstrap run, all rat trajectories came from rats that ran with only 2 other rats.
+
+#### BootMotherAnalyzeGroupsInCond.R
+
+Same as above, except that a rat in a “3 rat” bootstrap replicate could have come from a 15 rat condition, i.e. might have been actually in the box with 14 other rats.
+
