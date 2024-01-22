8762cf806a7304aba3271f3e08f60dcc65c8a31a
Michael R. Pasala <michaelpasala@Michaels-Air.attlocal.net>
2024-01-22 17:59:54 -0600

    speed wc 2

--- PlotSpeed.R
+++ PlotSpeed.R
@@ -22,14 +22,32 @@ combined_df <- data.frame(
   base = rep(NA, max)
 )
 
+food_drops <- c()
 
 for (i in 1:15) {
   result_subset <- subset(all_speeds, run == i)[0:max, 2:3]
+  first_occurrence_index <- which(result_subset$food == 1)[1]
+  food_drops <- c(food_drops, first_occurrence_index)
+  
   colnames(result_subset)[colnames(result_subset) == "food"] <- paste0("food_", i)
   colnames(result_subset)[colnames(result_subset) == "mean_speed"] <- paste0("ms", i)
   combined_df <- cbind(combined_df, result_subset)
+  
 }
 
 combined_df <- combined_df[-c(1)]
 
+earliest_drop <- min(food_drops)
+food_drop_diff <- food_drops - earliest_drop
+
+#nmy_data[[column_to_shift]] <- c(rep(NA, shift_up_by), my_data[[column_to_shift]][-(1:shift_up_by)])
+
+shift <- function(x, n){
+  c(x[-(seq(n))], rep(NA, n))
+}
+
+for (i in 1:15) {
+  combined_df[[paste0("food_", i)]] <- shift(combined_df[[ paste0("food_", i)]], food_drop_diff[i])[0:max]
+  combined_df[[paste0("ms", i)]] <- shift(combined_df[[paste0("ms", i)]], food_drop_diff[i])[0:max]
+}
 
