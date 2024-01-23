813f2418c1d890905995e0258cc682800e1c92a3
Michael R. Pasala <michaelpasala@Michaels-Air.attlocal.net>
2024-01-22 18:37:21 -0600

    speed rename

--- PlotSpeed_Food.R
+++ PlotSpeed_Food.R
@@ -1,9 +1,11 @@
 # remake of plot speed
+library(dplyr)
 
 load("/Users/michaelpasala/Research/MovementLab/analysis/speeds/3all_speeds.Rda")
 
 all_speeds$mean_speed <- rowMeans(all_speeds[, 1:3])
 all_speeds <- all_speeds[, 4:6]
+all_speeds$mean_speed[all_speeds$mean_speed == 0] <- NA
 
 #result_subset <- subset(all_speeds, run == 1)[0:500, 2:3]
 #colnames(result_subset)[colnames(result_subset) == "food"] <- paste0("food_", 1)
@@ -40,8 +42,6 @@ combined_df <- combined_df[-c(1)]
 earliest_drop <- min(food_drops)
 food_drop_diff <- food_drops - earliest_drop
 
-#nmy_data[[column_to_shift]] <- c(rep(NA, shift_up_by), my_data[[column_to_shift]][-(1:shift_up_by)])
-
 shift <- function(x, n){
   c(x[-(seq(n))], rep(NA, n))
 }
@@ -51,3 +51,29 @@ for (i in 1:15) {
   combined_df[[paste0("ms", i)]] <- shift(combined_df[[paste0("ms", i)]], food_drop_diff[i])[0:max]
 }
 
+pre <- combined_df[0:(earliest_drop-1), ]
+post <- combined_df[earliest_drop: max, ]
+
+pre <- pre %>%
+  select(-starts_with("food_"))
+
+post <- post %>%
+  select(-starts_with("food_"))
+
+pre$mean <- rowMeans(pre, na.rm = TRUE)
+post$mean <- rowMeans(post, na.rm = TRUE)
+
+plot_data <- data.frame(
+  minute = c(NA),
+  mean_speed = c(NA),
+)
+
+curr_min <- c()
+for (r in 1:nrows(pre)) {
+  mod <- r %% 61
+  if ( mod != 0) {
+    # within a minute
+    
+  }
+  
+}
