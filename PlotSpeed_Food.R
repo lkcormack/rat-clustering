fbf1732bb73d8347c6c587480f8e1276eea7c034
Michael R. Pasala <michaelpasala@Michaels-Air.attlocal.net>
2024-01-23 22:28:03 -0600

    finished new, variated box plot data and anova data agg

--- PlotSpeed_Food.R
+++ PlotSpeed_Food.R
@@ -51,28 +51,198 @@ for (i in 1:15) {
   combined_df[[paste0("ms", i)]] <- shift(combined_df[[paste0("ms", i)]], food_drop_diff[i])[0:max]
 }
 
+combined_df <- combined_df %>%
+  select(-starts_with("food_"))
+
+
 pre <- combined_df[0:(earliest_drop-1), ]
 post <- combined_df[earliest_drop: max, ]
 
 #pre <- combined_df
 
-pre <- pre %>%
-  select(-starts_with("food_"))
+#pre <- pre %>%
+#  select(-starts_with("food_"))
 
-post <- post %>%
-  select(-starts_with("food_"))
+#post <- post %>%
+#  select(-starts_with("food_"))
+
+pre <- pre[rev(row.names(pre)), ]
+
+############# new mean calc
+
+pre_exp_minutes = ceiling(nrow(pre) / bins_per_min)
+pre_minute_df <- data.frame(matrix(0, nrow = pre_exp_minutes, ncol = 1))
+
+post_exp_minutes = ceiling(nrow(post) / bins_per_min)
+post_minute_df <- data.frame(matrix(0, nrow = post_exp_minutes, ncol = 1))
+
+for (i in 1:15) {
+  
+  add_pre <- c()
+  add_post <- c()
+  
+  for (j in 1:(pre_exp_minutes) ) {
+    #print( ((j - 1) * bins_per_min + 1) ) 
+    #print( ((j - 1) * bins_per_min + 1) + bins_per_min - 1)
+    temp <- pre[((j - 1) * bins_per_min + 1):(((j - 1) * bins_per_min + 1) + bins_per_min - 1), i] 
+    #print("******")
+    #print(temp)
+    #print(mean(temp, na.rm = TRUE))
+    add_pre <- c(add_pre, mean(temp, na.rm = TRUE))
+  }
+  
+  for (j in 1:(post_exp_minutes) ) {
+    #print( ((j - 1) * bins_per_min + 1) ) 
+    #print( ((j - 1) * bins_per_min + 1) + bins_per_min - 1)
+    temp2 <- post[((j - 1) * bins_per_min + 1):(((j - 1) * bins_per_min + 1) + bins_per_min - 1), i] 
+    #print("******")
+    #print(temp)
+    #print(mean(temp, na.rm = TRUE))
+    add_post <- c(add_post, mean(temp2, na.rm = TRUE))
+  }
+
+  pre_minute_df[i] <- add_pre
+  post_minute_df[i] <- add_post
+  
+}
+
+pre_minute_df <- t(pre_minute_df)
+rownames(pre_minute_df) <- NULL
+
+pre_names <- as.character(seq(-1, (-1 * ncol(pre_minute_df))), 1)
+colnames(pre_minute_df) <- pre_names
+pre_minute_df <- pre_minute_df[, as.character(seq((-1 * ncol(pre_minute_df)), -1))]
+
+
+post_minute_df <- t(post_minute_df)
+rownames(post_minute_df) <- NULL
+
+post_names <- as.character(seq(0, ncol(post_minute_df) - 1), 1)
+colnames(post_minute_df) <- post_names
+
+final_df <- cbind(pre_minute_df, post_minute_df)
+
+boxplot(final_df,
+        ylim = c(0, 7),
+        xlab = "Minutes",  # Customize the x-axis label
+        ylab = "Mean Speed",  # Customize the y-axis label
+        main = glue("Condition {cond}")  # Add a plot title
+        #col = c("lightblue", "lightgreen", "lightpink")  # Specify box colors
+)
+
+save(final_df, file = glue("/Users/michaelpasala/Research/MovementLab/plots/speed/new/{cond}_speed_minute.RData"))
+
+
+############# load and trim data set values
+library(glue)
+cond <- 9
+load(glue("/Users/michaelpasala/Research/MovementLab/plots/speed/new/{cond}_speed_minute.RData"))
+keep <- as.character(seq(-5 ,5, 1))
+final_df <- final_df[, keep]
+save(final_df, file = glue("/Users/michaelpasala/Research/MovementLab/plots/speed/new/{cond}_speed_minute.RData"))
+
+
+############# graph data
+cond <- 9
+load(glue("/Users/michaelpasala/Research/MovementLab/plots/speed/new/{cond}_speed_minute.RData"))
+boxplot(final_df,
+        ylim = c(0, 7),
+        xlab = "Minutes",  # Customize the x-axis label
+        ylab = "Mean Speed",  # Customize the y-axis label
+        main = glue("Condition {cond}")  # Add a plot title
+        #col = c("lightblue", "lightgreen", "lightpink")  # Specify box colors
+)
+
+############# create anova friendly data
+cond<-3
+load(glue("/Users/michaelpasala/Research/MovementLab/plots/speed/new/{cond}_speed_minute.RData"))
+
+anova <- data.frame(
+  ID = c(NA),
+  Food = c(NA),
+  Time = c(NA),
+  Condition = c(NA),
+  Speed = c(NA)
+)
+
+for (i in 1:ncol(final_df)) {
+  time <- as.integer(colnames(final_df)[i])
+  for (j in 1:nrow(final_df)) {
+    
+    id <- paste0(j, "_", cond)
+    food <- ifelse(time < 0, "pre", "post")
+    condition <- cond
+    speed <- final_df[[j, i]]
+    
+    anova <- rbind(anova, c(id, food, time, condition, speed))
+  }
+}
+
+cond<-6
+load(glue("/Users/michaelpasala/Research/MovementLab/plots/speed/new/{cond}_speed_minute.RData"))
+
+for (i in 1:ncol(final_df)) {
+  time <- as.integer(colnames(final_df)[i])
+  for (j in 1:nrow(final_df)) {
+    
+    id <- paste0(j, "_", cond)
+    food <- ifelse(time < 0, "pre", "post")
+    condition <- cond
+    speed <- final_df[[j, i]]
+    
+    anova <- rbind(anova, c(id, food, time, condition, speed))
+  }
+}
+
+
+cond<-9
+load(glue("/Users/michaelpasala/Research/MovementLab/plots/speed/new/{cond}_speed_minute.RData"))
+
+
+for (i in 1:ncol(final_df)) {
+  time <- as.integer(colnames(final_df)[i])
+  for (j in 1:nrow(final_df)) {
+    
+    id <- paste0(j, "_", cond)
+    food <- ifelse(time < 0, "pre", "post")
+    condition <- cond
+    speed <- final_df[[j, i]]
+    
+    anova <- rbind(anova, c(id, food, time, condition, speed))
+  }
+}
+
+cond<-15
+load(glue("/Users/michaelpasala/Research/MovementLab/plots/speed/new/{cond}_speed_minute.RData"))
+
+
+for (i in 1:ncol(final_df)) {
+  time <- as.integer(colnames(final_df)[i])
+  for (j in 1:nrow(final_df)) {
+    
+    id <- paste0(j, "_", cond)
+    food <- ifelse(time < 0, "pre", "post")
+    condition <- cond
+    speed <- final_df[[j, i]]
+    
+    anova <- rbind(anova, c(id, food, time, condition, speed))
+  }
+}
+
+anova <- anova[-1, ]
+save(anova, file = "/Users/michaelpasala/Research/MovementLab/plots/speed/new/anova.RData")
+write.csv(anova, file = "/Users/michaelpasala/Research/MovementLab/plots/speed/new/anova.csv", row.names = FALSE)
+
+############# old mean calc
+# take 
 
 pre <- t(pre)
 post <- t(post)
 
-
-
-############
-# take 
 pre_mean <- data.frame(means = colMeans(pre, na.rm = TRUE))
 post_mean <- data.frame(means = colMeans(post, na.rm = TRUE))
 
-exp_minutes = nrow(post_mean) / bins_per_min
+post_exp_minutes = nrow(post_mean) / bins_per_min
 
 pre_minute_df <- data.frame(matrix(0, nrow = bins_per_min, ncol = 1))
 post_minute_df <- data.frame(matrix(0, nrow = bins_per_min, ncol = 1))
